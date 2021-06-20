e0fee932f27e9fa613c6756b5192360b29b14c51
kashishshahr <kashishshahr@gmail.com>
2021-06-20 19:51:35 +0530

    ExceptionHandling

--- C_Sharp/C_sharp_concepts/MoreAboutCSharp/ExceptionPractice.cs
+++ C_Sharp/C_sharp_concepts/MoreAboutCSharp/ExceptionPractice.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,12 @@ namespace MoreAboutCSharp
 {
     class ExceptionPractice
     {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hey");
+            string fileText=File.ReadAllText("E:/InternShip/c#/SI/C_Sharp/C_sharp_concepts/MoreAboutCSharp/SampleFiles/Data.txt");
+            Console.WriteLine(fileText);
+            Console.Read();
+        }
     }
 }
